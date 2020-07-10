9c3d7a44764940fec340e2f1491741d36852c122
Carl Wilke <carlwilke@outlook.com>
2020-07-10 19:43:41 +0100

    Adding EF to the project

---
+++ GunthersPieShop/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GunthersPieShop.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Pie> Pies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
