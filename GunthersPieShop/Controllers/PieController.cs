6e52095e50eff93830eb3b15be62650cff9661ab
Carl Wilke <carlwilke@outlook.com>
2020-07-11 15:50:24 +0100

    Added Home page

--- GunthersPieShop/Controllers/PieController.cs
+++ GunthersPieShop/Controllers/PieController.cs
@@ -31,5 +31,14 @@ namespace GunthersPieShop.Controller
             piesListViewModel.CurrentCategory = "Cheese cakes";
             return View(piesListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieByID(id);
+            if (pie == null)
+                return NotFound();
+
+            return View(pie);
+        }
     }
 }
