c62aa6928070b2d23db812fda02220c112bec09c
Carl Wilke <carlwilke@outlook.com>
2020-07-11 17:39:01 +0100

    Home page navigation added

--- GunthersPieShop/Controllers/HomeController.cs
+++ GunthersPieShop/Controllers/HomeController.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GunthersPieShop.Models;
+using GunthersPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GunthersPieShop.Controllers
 {
-    public class HomeController :Microsoft.AspNetCore.Mvc.Controller
+    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
     {
-        // GET: /<controller>/
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = _pieRepository.PiesOfTheWeek
+            };
+
+            return View(homeViewModel);
         }
     }
 }
---
+++ GunthersPieShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GunthersPieShop.Models;
+
+namespace GunthersPieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; set; }
+    }
+}
